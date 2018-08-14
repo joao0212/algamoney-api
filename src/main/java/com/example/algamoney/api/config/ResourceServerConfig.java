9e9939bd7b89e6a776c56a53eb798291b70b1d92
João Victor <jvc.martins12@gmail.com>
2018-08-13 23:48:23 -0300

    Ajustando o cors filter

--- src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -36,7 +37,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/categorias").permitAll()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				//.antMatchers("/categorias").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
